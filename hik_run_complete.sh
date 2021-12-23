35670f8c27848e4913e65881a2a8b909f60d5d9a
wangyingdong <wangyingdong@xiaomi.com>
2021-12-23 21:09:33 +0800

    增加多级路径补全问题

--- hik_run_complete.sh
+++ hik_run_complete.sh
@@ -1,49 +1,78 @@
-complete -F _myobj_complete_func hikrun
-_myobj_complete_func() {
-    local cur prev tmp_opt
+_hikrun() {
+    local pre cur tmp_opt
     . ${HIK_SCRIPT_TOP_DIR}/base.sh
     COMPREPLY=()
- 
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    cur=${COMP_WORDS[COMP_CWORD]}
 
     if [[ ${cur} == --* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
     fi
     if [[ ${COMP_CWORD} < 2 ]];then
-        COMPREPLY=( $(compgen -W "${opts1}" -- ${cur}) )
+        COMPREPLY=( $(compgen -W "${pre_opts}" -- ${cur}) )
         return 0
     fi
-    case "$prev" in
+
+    # case "$pre" in
+    # -f|-r )
+    #     COMPREPLY=( $( compgen -W "$READ_FORMAT" -- $cur ) )
+    #     return 0
+    #     ;;
+    # -t|-w )
+    #     COMPREPLY=( $( compgen -W "$WRITE_FORMAT" -- $cur ) )
+    #     return 0
+    # esac
+
+    # complete_options() {
+    #     local tmp_opts i
+    #     # opts="-f -r -t -w -o --output -v --version -h --help"
+    #     for i in "${COMP_WORDS[@]}"
+    #     do
+    #         if [ "$i" == "-B" -o "$i" == "-C" ]
+    #         then
+    #             opts="$opts"" -R -S --filter -p"
+    #             break
+    #         fi
+    #     done
+
+    #     for i in "${COMP_WORDS[@]}"
+    #     do
+    #         if [ "$i" == "-t" -o "$i" == "-w" ]
+    #         then
+    #             opts="$opts"" -s --template --toc"
+    #             break
+    #         fi
+    #     done
+    #     echo "$opts"
+    # }
+
+    case "$pre" in
       -B | -C)
         tmp_opt="android y4 rpi"
         COMPREPLY=( $(compgen -W "${tmp_opt}" -- ${cur}) )
         tmp_opt=""
-        ;;
-      -b | --build)
-        tmp_opt="`ls`"
-        COMPREPLY=( $(compgen -W "${tmp_opt}" -- ${cur}) )
-        tmp_opt=""
+        return 0
         ;;
       --code)
-      # echo  "${HIK_SCRIPT_TOP_DIR}/script/"
         local curd=`pwd`
         cd ${HIK_SCRIPT_TOP_DIR}/script/
         tmp_opt="`ls`"
         COMPREPLY=( $(compgen -W "${tmp_opt}" -- ${cur}) )
         tmp_opt=""
         cd ${curd} 
+        return 0
+        ;;
+      --set)
+        local curd=`pwd`
+        cd ${HIK_SCRIPT_TOP_DIR}/
         ;;
       --script)
-        local curd=`pwd`
-        cd ${HIK_SCRIPT_TOP_DIR}/script/
-        tmp_opt="`ls`"
-        cd ${curd} 
+        tmp_opt="`ls ${HIK_SCRIPT_TOP_DIR}/script/` ""`ls -F | grep "*"`"
         COMPREPLY=( $(compgen -W "${tmp_opt}" -- ${cur}) )
         tmp_opt=""
+        return 0
         ;;
     esac
-  return 0
 }
+complete -F _hikrun  -o default hikrun
